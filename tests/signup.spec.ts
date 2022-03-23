0b9e8e5fa55a2a9ea93a9c3582333e16bbe298d8
Artem Shoiko <shoiko.a.s@gmail.com>
2022-03-23 21:30:24 +0200

    Update env.example

--- tests/signup.spec.ts
+++ tests/signup.spec.ts
@@ -5,15 +5,15 @@ import { GoogleAuth } from '../pages/googleAuth'
 import Mailosaur from 'mailosaur'
 import MailosaurConfig from '../fixtures/mailosaurConfig'
 
-test.beforeEach(async ({ page }) => {
-  const iconPackPage = new IconPackPage(page)
-  await iconPackPage.open()
-})
 test.describe('Application UI Icon Pack', () => {
   const mailosaurServerId = MailosaurConfig.serverId
   const mailosaurServerDomain = MailosaurConfig.serverDomain
   const mailosaurApiKey = MailosaurConfig.apiKey
 
+  test.beforeEach(async ({ page }) => {
+    const iconPackPage = new IconPackPage(page)
+    await iconPackPage.open()
+  })
   test('Sign-up using MagicLink', async ({ page }) => {
     const iconPackPage = new IconPackPage(page)
     const signInModal = new SignInModal(page)
